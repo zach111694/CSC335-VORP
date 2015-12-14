7813da1096412aa521c521ad7969e9be36c4409b
Grant <gkuppelmeyer@lions.molloy.edu>
2015-12-14 01:07:26 -0500

    Made the first step of the algorithm by finding the max vorp possible with no budget implementation. Left some console.log()'s in there so I can see what was happening in the console of Google Chrome

--- events.js
+++ events.js
@@ -2,7 +2,7 @@ var computeButton = document.getElementById("computeButton");
 
 /*** FUNCTION THAT RETURNS AN ARRAY OF PLAYER OBJECTS ***/
 var create_player_objects = function(players,salaries){
-    var player_objects = [];
+    var player_objects = {};
 
     for(var i = 0; i < players.length;i++){
         var specific_player = players[i].split('\t');
@@ -15,16 +15,67 @@ var create_player_objects = function(players,salaries){
             specific_salary = salaries.substring(indexOf$ + 1, salaries.indexOf('\t', indexOf$));
             specific_salary = parseInt(specific_salary.replace(/,/,''));
 
-            player_objects.push(
-                {   player: specific_player[1],
-                    vorp: specific_player[16],
-                    position: specific_player[3],
-                    salary: specific_salary
-                }
-            );
+            player_objects[specific_player[1]] =
+            {
+                vorp: parseFloat(specific_player[16]),
+                position: specific_player[3],
+                salary: specific_salary
+            };
+
         }
     }
     return player_objects;
+};
+
+/*** VORP ALGORITHM FUNCTION !STILL IN PROGRESS. THIS IS JUST A STARTING TEMPLATE! ***/
+var create_position_object = function(player_objects){
+
+    var player_positions = {};
+
+    for(var i in player_objects){
+        var player = player_objects[i];
+
+        if(!(player.position in player_positions)){
+            player_positions[player.position] = {};
+        }
+
+        player_positions[player.position][i] = {
+            vorp: player.vorp,
+            salary: player.salary
+        };
+    }
+
+    //return player_positions ;
+    var maxVorpSoFar = 0;
+
+
+    for (var j in player_positions){
+        var base = player_positions[j];
+        var vorpArray = [];
+        for (var k in base){
+            vorpArray.push(base[k].vorp);
+            //console.log(base[k]);
+
+        }
+        //console.log(playerName);
+        console.log(vorpArray);
+
+        var maxVorpPerBase = Math.max.apply(null, vorpArray);
+        console.log(maxVorpPerBase);
+        maxVorpSoFar += maxVorpPerBase;
+
+
+        //maxVorpSoFar += Math.max(vorpObject);
+        //console.log(vorpObject);
+    }
+    return maxVorpSoFar;
+    //console.log(player_positions);
+
+
+
+
+
+
 };
 
 /*** MAIN FUNCTION ***/
@@ -44,11 +95,10 @@ computeButton.addEventListener("click", function() {
 
     // STORE RETURNED ARRAY OF OBJECTS FROM FUNCTION IN VARIABLE
     var player_objects = create_player_objects(data_with_vorp,data_with_salaries);
-
+    var position_objects = create_position_object(player_objects);
     /* Test alert function with JSON object inside param */
-    alert(JSON.stringify(player_objects));
+    //alert(JSON.stringify(player_objects));
+    alert(JSON.stringify(position_objects));
+
 
 });
-
-
-
